76cb8b3071a24c6fabc0c2254532cbe116342805
Matt Riley <mattrileyaus@gmail.com>
2020-07-22 14:25:20 +1000

    refactor build report tests

--- tests/build-report.test.js
+++ tests/build-report.test.js
@@ -4,40 +4,27 @@ const buildReport = require('../src/build-report');
 
 test('builds a success report', t => {
     const tasks = [
-        { name: 'A', points: 5 },
-        { name: 'B', points: 4 },
-        { name: 'C', points: 1 },
-        { name: 'D', points: 2 },
-        { name: 'E', points: 7 },
-        { name: 'F', points: 8 },
-        { name: 'G', points: 3 }
+        { name: 'A', points: 1 },
+        { name: 'B', points: 1 },
+        { name: 'C', points: 1 }
     ];
 
     const assignments = assignTasks(tasks, 3);
     const report = buildReport(tasks, assignments);
-
-    const expected = 'Task A: 5 points\nTask B: 4 points\nTask C: 1 points\nTask D: 2 points\nTask E: 7 points\nTask F: 8 points\nTask G: 3 points\nYes!\nChild 1: Task F (8 points) + Task D (2 points) = 10 points\nChild 2: Task E (7 points) + Task G (3 points) = 10 points\nChild 3: Task A (5 points) + Task B (4 points) + Task C (1 points) = 10 points';
-
+    const expected = 'Task A: 1 points\nTask B: 1 points\nTask C: 1 points\nYes!\nChild 1: Task A (1 points) = 1 points\nChild 2: Task B (1 points) = 1 points\nChild 3: Task C (1 points) = 1 points';
     t.deepEqual(report, expected);
     t.end();
 });
 
 test('builds a failure report', t => {
     const tasks = [
-        // { name: 'A', points: 5 },
-        { name: 'B', points: 4 },
-        { name: 'C', points: 1 },
-        { name: 'D', points: 2 },
-        { name: 'E', points: 7 },
-        { name: 'F', points: 8 },
-        { name: 'G', points: 3 }
+        { name: 'A', points: 1 },
+        { name: 'B', points: 1 }
     ];
 
     const assignments = assignTasks(tasks, 3);
     const report = buildReport(tasks, assignments);
-
-    const expected = 'Task B: 4 points\nTask C: 1 points\nTask D: 2 points\nTask E: 7 points\nTask F: 8 points\nTask G: 3 points\nNo!';
-
+    const expected = 'Task A: 1 points\nTask B: 1 points\nNo!';
     t.deepEqual(report, expected);
     t.end();
 });
