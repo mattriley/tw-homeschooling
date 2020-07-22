307404abac775b13d4408bc9963251d2555bca33
Matt Riley <mattrileyaus@gmail.com>
2020-07-22 14:28:47 +1000

    refactor parse args tests

--- tests/parse-args.test.js
+++ tests/parse-args.test.js
@@ -2,77 +2,61 @@ const test = require('tape');
 const parseArgs = require('../src/parse-args');
 
 test('parses task names and points', t => {
-    const args = ['A5', 'B4', 'C1', 'D2', 'E7', 'F8', 'G3'];
+    const args = ['A1', 'B2', 'C3'];
     const { tasks } = parseArgs(args);
 
     const expectedTasks = [
-        { name: 'A', points: 5 },
-        { name: 'B', points: 4 },
-        { name: 'C', points: 1 },
-        { name: 'D', points: 2 },
-        { name: 'E', points: 7 },
-        { name: 'F', points: 8 },
-        { name: 'G', points: 3 }
+        { name: 'A', points: 1 },
+        { name: 'B', points: 2 },
+        { name: 'C', points: 3 }
     ];
 
     t.deepEqual(tasks, expectedTasks);
     t.end();
 });
 
-test('assigns unique task names if name not provided', t => {
-    const args = ['5', '4', '1', '2', '7', '8', '3'];
+test('assigns unique task names when no names provided', t => {
+    const args = ['1', '2', '3'];
     const { tasks } = parseArgs(args);
 
     const expectedTasks = [
-        { name: 'A', points: 5 },
-        { name: 'B', points: 4 },
-        { name: 'C', points: 1 },
-        { name: 'D', points: 2 },
-        { name: 'E', points: 7 },
-        { name: 'F', points: 8 },
-        { name: 'G', points: 3 }
+        { name: 'A', points: 1 },
+        { name: 'B', points: 2 },
+        { name: 'C', points: 3 }
     ];
 
     t.deepEqual(tasks, expectedTasks);
     t.end();
 });
 
-test('combination of named and unnamed not allowed', t => {
-    const args = ['A5', '4', 'C1', '2', 'E7', '8', 'F3'];
+test('combination of named and unnamed tasks not allowed', t => {
+    const args = ['A1', '2', '3'];
     const { error } = parseArgs(args);
-
     const expectedError = 'Blank name found. Tasks must be all named or unnamed.';
-
     t.deepEqual(error, expectedError);
     t.end();
 });
 
-test('duplicate names not allowed', t => {
-    const args = ['A5', 'A4', 'B1', 'B2'];
+test('duplicate tasks names not allowed', t => {
+    const args = ['A1', 'A2', 'A3'];
     const { error } = parseArgs(args);
-
     const expectedError = 'Duplicate names found. Task names must be unique.';
-
     t.deepEqual(error, expectedError);
     t.end();
 });
 
 test('blank points not allowed', t => {
-    const args = ['A', 'B'];
+    const args = ['A', 'B2', 'C3'];
     const { error } = parseArgs(args);
-
     const expectedError = 'Blank or zero points found. Points must be greater than zero.';
-
     t.deepEqual(error, expectedError);
     t.end();
 });
 
 test('zero points not allowed', t => {
-    const args = ['A0', 'B0'];
+    const args = ['A0', 'B2', 'B3'];
     const { error } = parseArgs(args);
-
     const expectedError = 'Blank or zero points found. Points must be greater than zero.';
-
     t.deepEqual(error, expectedError);
     t.end();
 });
