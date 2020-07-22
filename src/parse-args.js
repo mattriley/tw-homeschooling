78a658082f90d4919a34cb9ae99935bdef636408
Matt Riley <mattrileyaus@gmail.com>
2020-07-22 12:19:32 +1000

    assigns unique task names if name not provided

--- src/parse-args.js
+++ src/parse-args.js
@@ -1,12 +1,21 @@
 module.exports = args => {
 
-    return args.map(arg => {
-        const { name, points } = arg.match(/^(?<name>.+)(?<points>\d+)$/).groups;
-        return {
-            name: name.trim(),
-            points: parseInt(points)
-        };
+    const assignUniqueTaskNames = tasks => {
+        return tasks.map((task, i) => {
+            const newName = String.fromCharCode(65 + i) + task.name;
+            return { ...task, name: newName };
+        });
+    };
+
+    const tasks = args.map(arg => {
+        const [points] = arg.match(/\d+$/);
+        const name = arg.slice(0, -points.length).trim();
+        return { name, points: parseInt(points) };
     });
+
+    const names = tasks.map(t => t.name);
+    const uniquelyNamed = new Set(names).size === tasks.length;
+    return uniquelyNamed ? tasks : assignUniqueTaskNames(tasks);
 
 };
 
--- tests/parse-args.test.js
+++ tests/parse-args.test.js
@@ -1,12 +1,48 @@
 const test = require('tape');
 const parseArgs = require('../src/parse-args');
 
-test('builds a success report', t => {
+test('parses task names and points', t => {
     const args = ['A5', 'B4', 'C1', 'D2', 'E7', 'F8', 'G3'];
     const tasks = parseArgs(args);
 
     const expectedTasks = [
         { name: 'A', points: 5 },
+        { name: 'B', points: 4 },
+        { name: 'C', points: 1 },
+        { name: 'D', points: 2 },
+        { name: 'E', points: 7 },
+        { name: 'F', points: 8 },
+        { name: 'G', points: 3 }
+    ];
+
+    t.deepEqual(tasks, expectedTasks);
+    t.end();
+});
+
+test('assigns unique task names if name not provided', t => {
+    const args = ['5', '4', '1', '2', '7', '8', '3'];
+    const tasks = parseArgs(args);
+
+    const expectedTasks = [
+        { name: 'A', points: 5 },
+        { name: 'B', points: 4 },
+        { name: 'C', points: 1 },
+        { name: 'D', points: 2 },
+        { name: 'E', points: 7 },
+        { name: 'F', points: 8 },
+        { name: 'G', points: 3 }
+    ];
+
+    t.deepEqual(tasks, expectedTasks);
+    t.end();
+});
+
+test('prepends letter to name when any are not unique', t => {
+    const args = ['FOO5', '4', '1', '2', '7', '8', '3'];
+    const tasks = parseArgs(args);
+
+    const expectedTasks = [
+        { name: 'AFOO', points: 5 },
         { name: 'B', points: 4 },
         { name: 'C', points: 1 },
         { name: 'D', points: 2 },
